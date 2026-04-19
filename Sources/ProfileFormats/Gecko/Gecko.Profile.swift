5d6b1ab35da754f011ff93ede8e8d629c9308cb4
Diana Ma <diana@rarestype.com>
2026-04-19 06:34:55 +0000

    add demangling support

--- Sources/ProfileFormats/Gecko/Gecko.Profile.swift
+++ Sources/ProfileFormats/Gecko/Gecko.Profile.swift
@@ -1,9 +1,22 @@
+internal import JQ
 public import JSON
 
 extension Gecko {
     @frozen public struct Profile {
         public let threads: [Thread]
         public let shared: Shared
+    }
+}
+extension Gecko.Profile: SwiftDemanglableProfile {
+    public static func resymbolicate(
+        json: inout JSON.Node,
+        by transform: (consuming String) -> String
+    ) throws {
+        try json["shared"]["stringArray"][] &? {
+            for i: Int in $0.indices {
+                $0.elements[i] = .string(transform(try $0[i].decode()))
+            }
+        }
     }
 }
 extension Gecko.Profile: JSONObjectDecodable {
